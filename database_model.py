bc0a83585e29e00be8f469737474f9db21866384
Benwong410 <bw410@connect.hku.hk>
2023-11-13 18:12:44 +0800

    connect db in database_model

--- database_model.py
+++ database_model.py
@@ -1,41 +1,53 @@
 # Ben: implement get function to pull data from database
 from config import *
 
+##### Template
+        # mycursor = conn.cursor()
+        # sql = ""
+        # mycursor.execute(sql)
+        # result = mycursor.fetchall()
+        # return result   
+
 
 ############################ Pull Data from DB #############################
-# db connection function
-def connect_db():
-    pass
+class database_model:
+    # db connection function
+    def connect_db():
+        conn = mysql.connector.connect(user=CONFIG_USER, password=CONFIG_PASSWORD,host=CONFIG_HOST,database=CONFIG_DATABASE)
+        return conn
 
-# db disconnection function
-def disconnect_db():
-    pass
+    ## Usage: for the redirection of the mainpage button 
+    ## Variable: user_id
+    ## Return: is there any course that will be started in an hours
+    ## Return Type: Boolean
+    def get_is_course_start_in_an_hour(conn, user_id):
+        pass
+        ##### Template
+        # mycursor = conn.cursor()
+        # sql = ""
+        # mycursor.execute(sql)
+        # result = mycursor.fetchall()
+        # return result   
 
-## Usage: for the redirection of the mainpage button 
-## Return: is there any course that will be started in an hours
-## Return Type: Boolean
-def get_is_course_start_in_an_hour():
-    pass
+    ## Usage: for the redirection of the mainpage button 
+    ## Return: is there any course that will be started in an hours
+    ## Return Type: Array
+    def get_user_data():
+        pass
 
-## Usage: for the redirection of the mainpage button 
-## Return: is there any course that will be started in an hours
-## Return Type: Array
-def get_user_data():
-    pass
+    ## Usage: get course data in course table
+    ## Return: ....
+    ## Return Type: Array
+    def get_course_data():
+        pass
 
-## Usage: get course data in course table
-## Return: ....
-## Return Type: Array
-def get_course_data():
-    pass
+    ## Usage: get course time table of an user
+    ## Return: ....
+    ## Return Type: Array
+    def get_course_timetable():
+        pass
 
-## Usage: get course time table of an user
-## Return: ....
-## Return Type: Array
-def get_course_timetable():
-    pass
-
-############################ Update Data from DB #############################
-##Usage: Update the login time of user after login
-def update_login_time():
-    pass
+    ############################ Update Data from DB #############################
+    ##Usage: Update the login time of user after login
+    def update_login_time():
+        pass
