5c17b436e2a0aef13ddee32fabc0d489674268b3
Benwong410 <bw410@connect.hku.hk>
2023-11-13 18:47:20 +0800

    sql dummy function

--- database_model.py
+++ database_model.py
@@ -10,44 +10,53 @@ from config import *
 
 
 ############################ Pull Data from DB #############################
-class database_model:
     # db connection function
-    def connect_db():
-        conn = mysql.connector.connect(user=CONFIG_USER, password=CONFIG_PASSWORD,host=CONFIG_HOST,database=CONFIG_DATABASE)
-        return conn
+def connect_db():
+    conn = mysql.connector.connect(user=CONFIG_USER, password=CONFIG_PASSWORD,host=CONFIG_HOST,database=CONFIG_DATABASE)
+    return conn
 
     ## Usage: for the redirection of the mainpage button 
     ## Variable: user_id
     ## Return: is there any course that will be started in an hours
     ## Return Type: Boolean
-    def get_is_course_start_in_an_hour(conn, user_id):
-        pass
-        ##### Template
-        # mycursor = conn.cursor()
-        # sql = ""
-        # mycursor.execute(sql)
-        # result = mycursor.fetchall()
-        # return result   
+def get_is_course_start_in_an_hour(conn):
+        #### Dummy Template
+        mycursor = conn.cursor()
+        sql = """select count(*) as is_class from Registercourses rc 
+        join Coursetimeslots cs on rc.course_id = cs.course_id 
+        where rc.user_id = '1' 
+        and  cs.start_time = '11:00'
+        and cs.day_in_week = '2'
+        """
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        for row in result:
+            return row[0]
 
     ## Usage: for the redirection of the mainpage button 
     ## Return: is there any course that will be started in an hours
     ## Return Type: Array
-    def get_user_data():
-        pass
+def get_user_data():
+    pass
 
     ## Usage: get course data in course table
     ## Return: ....
     ## Return Type: Array
-    def get_course_data():
-        pass
+def get_course_data():
+    pass
 
     ## Usage: get course time table of an user
     ## Return: ....
     ## Return Type: Array
-    def get_course_timetable():
-        pass
+def get_course_timetable():
+    pass
 
     ############################ Update Data from DB #############################
     ##Usage: Update the login time of user after login
-    def update_login_time():
-        pass
+def update_login_time():
+    pass
+
+
+# Testing
+# conn= connect_db()
+# print(get_is_course_start_in_an_hour(conn))
